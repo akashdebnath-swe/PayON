4ad30fa3253b0cc8ab86c6a192f76ce9463c086f
theaakashd <akashdebnathwd@gmail.com>
2024-12-26 22:52:57 +0530

    First Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const fontSans = Inter({
+    subsets: ["latin"],
+    variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+    title: "Docu Mart",
+    description: "A e-commerce website where you can sell your notes.",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body
+                className={cn(
+                    "min-h-screen bg-background font-sans antialiased",
+                    fontSans.variable
+                )}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
